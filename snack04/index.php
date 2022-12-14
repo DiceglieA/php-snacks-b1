d386308c95c7f32d1905e1af3d409a5168ffd85b
DiceglieA <angelodiceglie10@gmail.com>
2022-12-14 15:39:22 +0100

    numeri randomici snack 4

--- snack04/index.php
+++ snack04/index.php
@@ -1,2 +1,13 @@
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+$numeri = [];
+
+for ($i=0; $i < 15; $i++) { 
+    $numran = rand(1, 50);
+    var_dump($numran);
+}
+
+?>
